cc1640a3c8bcd9d0d23b2891534b5366fe45e9a0
aodong.qin <ioperations.c@gmail.com>
2025-08-03 12:00:04 +0800

    custom

--- custom.el
+++ custom.el
@@ -1,17 +1,20 @@
+(put 'customize-face 'disabled nil)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(git-gutter:added-sign "☀")
- '(git-gutter:deleted-sign "☂")
- '(git-gutter:modified-sign "☁")
- '(git-gutter:window-width 2)
- '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- '(package-selected-packages '(centaur-tabs yascroll lsp-ui)))
+ '(safe-local-variable-values
+   '((eval c-set-offset 'innamespace 0)
+     (whitespace-line-column . 80))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(avy-lead-face ((t (:background "wheat" :foreground "#000000" :weight bold))))
+ '(ccls-sem-global-variable-face ((t (:underline t :weight extra-bold))))
+ '(lsp-face-highlight-read ((t (:background "sea green"))))
+ '(lsp-face-highlight-write ((t (:background "brown4"))))
+ '(lsp-ui-sideline-current-symbol ((t (:foreground "grey38" :box nil))))
+ '(lsp-ui-sideline-symbol ((t (:foreground "grey30" :box nil)))))
